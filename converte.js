1728f8bdf64ab2e871b123b9010ad4467f40fff5
andreiasaling <andreiasamira@gmail.com>
2022-06-17 14:46:36 -0300

    upload files

---
+++ converte.js
@@ -0,0 +1,22 @@
+const inputKg = document.querySelector('#inputKg');
+const listaResultado = document.querySelector('#saida');
+const botaoLimpar = document.querySelector('#botaoLimpar');
+
+
+function converteMedidas() {
+    let valorKg = document.querySelector('#inputKg').value;
+    document.querySelector('#grama').textContent = valorKg * 1000;
+    document.querySelector('#miligrama').textContent = valorKg * 1000000;
+    document.querySelector('#tonelada').textContent = valorKg * 0.001;
+
+    listaResultado.removeAttribute('hidden');
+    botaoLimpar.removeAttribute('hidden');
+}
+
+
+botaoLimpar.addEventListener('click',function(){
+    listaResultado.setAttribute('hidden','hidden');
+    inputKg.textContent = '';
+    inputKg.focus();
+    botaoLimpar.setAttribute('hidden','hidden');
+});
